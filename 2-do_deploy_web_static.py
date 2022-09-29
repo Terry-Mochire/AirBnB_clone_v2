81a7281e66063f3a9259940d4bd04d680d0ad3ba
Terry <terrybmochire@gmail.com>
2022-09-29 19:04:51 +0300

    Correct task 2 - Static deployment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
-from fabric.api import *
-from datetime import datetime
-import os
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
 
+from fabric.api import put, run, env
+from os.path import exists
 env.hosts = ['3.238.28.101', '3.209.82.116']
 
 
-
 def do_deploy(archive_path):
-    """ fabric script to deploy to a server """
-    if not os.path.exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
-    filename = archive_path.split("/")
-    filename = filename[1]
-    fname = filename.split('.')
-    fname = fname[0]
-
-    newpath = '/data/web_static/releases/{}/'.format(fname)
-
     try:
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(newpath))
-        run("tar -xzf /tmp/{} -C {}".format(filename, newpath))
-        run("rm /tmp/{}".format(filename))
-        run("mv {}web_static/* {}".format(newpath, newpath))
-        run("rm -rf {}web_static".format(newpath))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(newpath))
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception:
+    except:
         return False
