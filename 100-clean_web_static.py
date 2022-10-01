2bd37be3d4bee42b0cdcd4fe23971cb5cd937247
Terry <terrybmochire@gmail.com>
2022-10-01 11:00:32 +0300

    Edit pycodestyle.

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,13 +8,12 @@ def do_clean(number=0):
     """deletes out of data archives"""
 
     number = int(number)
+    command = "cd /data/web_static/releases; ls | head -n -{} | xargs rm -rf"
     if number == 1 or number == 0:
         local("cd versions; ls | head -n -1 | xargs rm -rf")
         run("cd /data/web_static/releases; ls | head -n -1 | xargs rm -rf")
     else:
         local("cd versions; ls | head -n -{} | xargs rm -rf".format(number))
         run(
-            "cd /data/web_static/releases; ls | head -n -{} | xargs rm -rf".format(
-                number
-            )
+            command.format(number)
         )
