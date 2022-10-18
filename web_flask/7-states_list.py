43eff274b204f33d23eb8a69e4e9583619c4525c
Terry <terrybmochire@gmail.com>
2022-10-18 17:36:36 +0300

    Task 8.

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: Displays a HTML page with a list of all State objects
-    stored in DBStorage sorted by name
-"""
-from flask import Flask, render_template
+""" Starts a Flash Web Application """
 from models import storage
 from models.state import State
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Displays a HTML page with a list of all State objects
-    stored in DBStorage sorted by name
-    """
-    states = storage.all(State).values()
-    return render_template("7-states_list.html", states=states)
-
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the storage on teardown"""
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
     storage.close()
 
 
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ displays a HTML page with a list of states """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    return render_template('7-states_list.html', states=states)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
