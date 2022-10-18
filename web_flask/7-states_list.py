97d836aa557b8b595c9f7d160236c239358a08bb
Terry <terrybmochire@gmail.com>
2022-10-18 17:21:03 +0300

    Task 8.

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states_list: Displays a HTML page with a list of all State objects
+    stored in DBStorage sorted by name
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays a HTML page with a list of all State objects
+    stored in DBStorage sorted by name
+    """
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
